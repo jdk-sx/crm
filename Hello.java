1cfcb43a189245310a67e9ddb252e7fbcc8b9bda
mldn <mldnqa@163.com>
2016-06-27 14:46:30 +0800

    Change Hello.java Add Two Lines

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Hello GIT") ;
+		System.out.println("www.mldn.cn") ;
+		System.out.println("good job") ;
 	}
 }
