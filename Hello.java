f2f22424e33203efe7c11a310521f47efeb9f1ae
mldn <mldnqa@163.com>
2016-07-18 08:46:06 +0800

    Change Hello.java And Add Admin.java

--- Hello.java
+++ Hello.java
@@ -4,5 +4,7 @@ public class Hello {
 		System.out.println("www.mldn.cn") ;
 		System.out.println("good job") ;
 		System.out.println("nihao ") ;
+		System.out.println("极限IT：www.jixianit.com") ;
+		System.out.println("世界和平，没有战争！") ;
 	}
 }
